ef494a6989d08d408c2534bcae1d48e469a47afc
yominmirin <longdoumanwa@gmail.com>
2025-07-21 17:26:49 +0900

    Itemモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_18_092545) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_21_081215) do
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "info", null: false
+    t.integer "category_id", null: false
+    t.integer "sales_status_id", null: false
+    t.integer "shipping_fee_status_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "scheduled_delivery_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_18_092545) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
