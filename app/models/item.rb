c56a89fd7788e44be924a2249ae9c8e96d510104
yominmirin <longdoumanwa@gmail.com>
2025-07-22 14:20:41 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 end
