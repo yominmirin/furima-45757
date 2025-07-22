2180246ffe120c6084e982d413740381adfc407d
yominmirin <longdoumanwa@gmail.com>
2025-07-22 17:27:17 +0900

    出品失敗時機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
