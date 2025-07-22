625f807502a6b27b0e741640a6a5dd416a59ad13
yominmirin <longdoumanwa@gmail.com>
2025-07-22 17:16:35 +0900

    販売手数料、利益の計算機能の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,21 @@
+window.addEventListener('turbo:load', () =>{
+  const priceInput = document.getElementById("item-price");
+  const taxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  if (!priceInput) return;
+
+  priceInput.addEventListener("input", () => {
+    const price = Number(priceInput.value);
+
+    if (price >= 300 && price <= 9999999) {
+      const fee = Math.floor(price * 0.1);
+      const gain = price - fee;
+      taxPrice.textContent = fee;
+      profit.textContent = gain;
+    } else {
+      taxPrice.textContent = "";
+      profit.textContent = "";
+    };
+  });
+});
