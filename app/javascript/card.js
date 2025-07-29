da10f8033318462074fc005e3edb933a1e19f016
yominmirin <longdoumanwa@gmail.com>
2025-07-29 18:33:33 +0900

    turboイベントの追加

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -30,4 +30,5 @@ const pay = () => {
 };
 
 window.addEventListener("load", pay);
-window.addEventListener("render", pay);
+window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
