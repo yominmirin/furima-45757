b62025d74891ee086de44e455feff432e477de95
yominmirin <longdoumanwa@gmail.com>
2025-07-29 18:34:33 +0900

    Factoryの修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -6,8 +6,6 @@ FactoryBot.define do
     street_address { '東京' }
     building { '東京' }
     phone_number { '09001234567' }
-    user_id         { FactoryBot.create(:user).id }
-    item_id         { FactoryBot.create(:item).id }
     token           { 'tok_abcdefghijk00000000000000000' }
   end
 end
